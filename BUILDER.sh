8af8092463abe4c8893f51ad0990c78d4baf40a3
deebloo <dannybluedesign@gmail.com>
2019-02-05 21:19:53 -0500

    better builder

--- BUILDER.sh
+++ BUILDER.sh
@@ -3,11 +3,19 @@
 ACTION=$1
 PROJECT=$2
 
-source "packages/$PROJECT/BUILDER.sh"
-
-if type $ACTION 2>/dev/null | grep -q 'function'
+if [ -z "$PROJECT" ]
 then
-    $ACTION ${@:3}
+    for DIR in `find ./packages -mindepth 1 -maxdepth 1 -type d`
+    do
+        ./BUILDER.sh "$ACTION" $(basename "$DIR")
+    done
 else
-    echo "Project \"$PROJECT\" does not define builder method \"$ACTION\""
+    source "packages/$PROJECT/BUILDER.sh"
+    
+    if type $ACTION 2>/dev/null | grep -q 'function'
+    then
+        $ACTION ${@:3}
+    else
+        echo "Project \"$PROJECT\" does not define builder method \"$ACTION\""
+    fi
 fi
--- tools/RELEASE_ALL.sh
+++
@@ -1,4 +0,0 @@
-for DIR in `find ./packages -mindepth 1 -maxdepth 1 -type d`
-do
-    npm run release $(basename "$DIR")
-done
