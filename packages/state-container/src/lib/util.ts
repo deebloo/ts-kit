10566c71d21ff88cbf0932a4a52c8f8c631885ed
deebloo <dannybluedesign@gmail.com>
2019-02-14 21:14:40 -0500

    add utils

--- packages/state-container/src/lib/util.ts
+++ packages/state-container/src/lib/util.ts
@@ -2,5 +2,5 @@ import { take } from 'rxjs/operators';
 
 import { StateContainer } from './state-container';
 
-export const getSnapshot = async (container: StateContainer<any>) =>
-  await container.value.pipe(take(1)).toPromise();
+export const getSnapshot = <T>(container: StateContainer<T>) =>
+  container.value.pipe(take(1)).toPromise();
