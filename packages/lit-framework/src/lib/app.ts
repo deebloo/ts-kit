2424bc57a2c61832f4e412eba9689b458a0c7e3b
deebloo <dannybluedesign@gmail.com>
2019-11-02 21:29:45 -0400

    chore: fix builds

--- packages/lit-framework/src/lib/app.ts
+++ packages/lit-framework/src/lib/app.ts
@@ -1,8 +1,4 @@
 import { Injector } from '@ts-kit/di';
-import { html as litHtml } from 'lit-html';
-
-export const html = (strings: TemplateStringsArray, ...values: unknown[]) =>
-  litHtml(strings, ...values);
 
 export const bootstrapApplication = () => {
   window.ROOT__INJECTOR__ = new Injector();
